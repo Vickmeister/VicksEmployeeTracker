d08c8bc54cb681a04dff3bbcb5c059358aa48d62
Vick Manjarrez <vick.manjarrez@gmail.com>
2020-10-02 21:52:16 -0500

    updating function

--- seeds.sql
+++ seeds.sql
@@ -31,4 +31,4 @@ INSERT INTO roleTable (title, salary, department_id)
 VALUES ("General Manager", 2000000, 1), ("Head Coach", 3000000, 2), ("Team Captains", 23000000, 3);
 
 INSERT INTO employeeTable (firstName, lastName, role_id, manager_id)
-VALUES ("Ryan", "Pace", 1, 1), ("Matt", "Nagy", 3, 2), ("Khalil", "Mack", 2, 3);
+VALUES ("Ryan", "Pace", 1, 1), ("Matt", "Nagy", 2, 2), ("Khalil", "Mack", 3, 3);
